d06cb2ca0efd9bd61c7e32be7e311d4331671094
Mykola <mykola.maksymov@gmail.com>
2021-11-04 16:53:21 +0100

    Learning C#

---
+++ Console applications/Learning/Person_Class.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Learning
+{
+    
+
+    class Person_Class
+    {
+        public int Height { get; set; }
+        public string Education { get; set; }
+
+    }
+}
